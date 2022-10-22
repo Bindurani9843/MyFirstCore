de2ff35ef9096bab4b85656640fb3d34e2b1e010
Bindu <bindurani9843@gmail.com>
2022-10-22 15:26:15 +0530

    Add project files.

---
+++ MyFirstCore/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyFirstCore.Models
+{
+    public partial class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public int? Price { get; set; }
+        public string? Color { get; set; }
+        public int? Quantity { get; set; }
+    }
+}
